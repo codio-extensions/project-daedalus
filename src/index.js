f0741b42262cb79864ab78bf5636b8c3760f5b80
Mikhail Loginov <mloginov@codio.com>
2026-04-09 13:47:22 +0100

    create bundle(index.js) from sources add dev build(creates dev server on http://localhost:1234 by default)

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import * as ext1 from "./extension-1/index"
+import * as ext2 from "./extension-2/index"
+
+// Wrapping the whole extension in a JS function
+// (ensures all global variables set in this extension cannot be referenced outside its scope)
+(async function(codioIDE, window) {
+
+  // ── EXTENSION SETUP ────────────────────────────────────────────────────────
+
+  // Alias for convenience
+  const coachAPI = codioIDE.coachBot
+
+  // Register the top-level entry point for this extension
+  coachAPI.register("contentAssistantsMenuButton", "Content Assistants Menu", showMenuButtons)
+
+  function showMenuButtons() {
+    coachAPI.showButton("1. Generate Learning Objectives", onAssistantOneButtonPress)
+    coachAPI.showButton("2. Generate Alt Text for All Images", onAssistantTwoButtonPress)
+  }
+
+  async function onAssistantOneButtonPress() {
+    await ext1.generateLearningObjectives()
+    showMenuButtons()
+  }
+
+  async function onAssistantTwoButtonPress() {
+    await ext2.startAltTextGeneration()
+    showMenuButtons()
+  }
+// calling the function immediately by passing the required variables
+})(window.codioIDE, window)
